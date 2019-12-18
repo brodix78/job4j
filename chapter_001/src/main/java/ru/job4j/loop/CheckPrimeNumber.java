10e58d65be296091886a4874bfe8c3173b852df7
Ilya Broodnikov <brodnikov1950@gmail.com>
2019-12-18 22:02:00 +0400

    Простое число.[#210182]

---
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,14 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+
+    public static boolean check(int finish) {
+        int lastcheck = finish / 2;
+        for (int i = 2; i <= lastcheck; i++) {
+            if ((finish%i) == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
