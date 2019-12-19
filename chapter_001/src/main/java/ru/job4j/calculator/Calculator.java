b60b12efb1fcc28f9683f14dc9ca2da154da95cc
Ilya Brodnikov <brodnikov1950@gmail.com>
2019-12-19 16:54:50 +0400

    JavaDoc. Code convention[#210187]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,22 +1,57 @@
 package ru.job4j.calculator;
 
+/**
+ *Calculator
+ *
+ * @athor Ilya Brodnikov
+ */
+
 public class Calculator {
+
+    /**
+     * Method add
+     * @param first and second numbers
+     * @return sum of numbers
+     */
     public static void add(int first, int second) {
         int result = first + second;
         System.out.println(first + " + " + second + " = " + result);
     }
+
+    /**
+     * Method div
+     * @param first and second numbers
+     * @return quotient of numbers
+     */
     public static void div(int first, int second) {
         int result = first / second;
         System.out.println(first + " / " + second + " = " + result);
     }
+
+    /**
+     * Method multiply
+     * @param first and second numbers
+     * @return product of numbers
+     */
     public static void multiply(int first, int second) {
         int result = first * second;
         System.out.println(first + " * " + second + " = " + result);
     }
+
+    /**
+     * Method subtract
+     * @param first and second numbers
+     * @return difference of numbers
+     */
     public static void subtract(int first, int second) {
         int result = first - second;
         System.out.println(first + " - " + second + " = " + result);
     }
+
+    /**
+     * Calculation with two numbers
+     * @param args
+     */
     public static void main(String[] args) {
         add(1, 1);
         div(4, 2);
---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,9 @@
+package ru.job4j.calculator;
+
+/**
+ * Package for calculate task.
+ *
+ * @author Ilya Brodnikov (brodnikov1950@ygmail.com)
+ * @version 1.0
+ * @since 0.1
+ */
