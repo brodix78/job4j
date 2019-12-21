c0a62f0e984052d093d93f832fb90bdf86773253
Ilya Broodnikov <brodnikov1950@gmail.com>
2019-12-21 19:39:42 +0400

    Дефрагментация массива.[#210134]

---
+++ chapter_001/src/main/java/ru/job4j/array/Defragment.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+public class Defragment {
+    public static String[] compress(String[] array) {
+        for (int index = 0; index < array.length; index++) {
+            String cell = array[index];
+            if (cell == null) {
+                int in = index;
+                while (in < array.length-1) {
+                    array[in] = array[in + 1];
+                    in++;
+                }
+                array[array.length - 1] = null;
+            }
+            System.out.print(array[index] + " ");
+        }
+        return array;
+    }
+}
