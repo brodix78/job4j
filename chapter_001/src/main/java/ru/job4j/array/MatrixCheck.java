e092fe1982107580cfccdf9bfa6d77d8669f3c4e
Ilya Broodnikov <brodnikov1950@gmail.com>
2019-12-21 14:16:30 +0400

    Выигрышные комбинации в сокобан[#210127]

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -1,8 +1,66 @@
 package ru.job4j.array;
 
 public class MatrixCheck {
+    public static boolean monoHorizontal(char[][] board, char index) {
+        boolean result = false;
+        for (int x = 0; x < board.length; x++) {
+            if (board[x][0] == index) {
+                result = true;
+                for (int y = 0; y < board[x].length; y++) {
+                    if (board[x][y] != index) {
+                        result = false;
+                        break;
+                    }
+                }
+            }
+            if (result == true) {
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static boolean monoVertical(char[][] board, char index) {
+        boolean result = false;
+        for (int y = 0; y < board[0].length; y++) {
+            if (board[0][y] == index) {
+                result = true;
+                for (int x = 0; x < board.length; x++) {
+                    if (board[x][y] != index) {
+                        result = false;
+                        break;
+                    }
+                }
+            }
+            if (result == true) {
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static boolean monoDiagonal(char[][] board, char index) {
+        boolean resultLeft = true;
+        boolean resultRight = true;
+        for (int x = 0; x < board.length; x++) {
+            if (board[x][x] != index) {
+                resultLeft = false;
+            }
+            if (board[x][board.length - x - 1] != index) {
+                resultRight = false;
+            }
+        }
+        if (resultLeft == false) {
+            resultLeft = resultRight;
+        }
+            return resultLeft;
+    }
+
     public static boolean isWin(char[][] board) {
         boolean result = false;
+        if (monoHorizontal(board, 'X') || monoVertical(board, 'X') ||monoDiagonal(board, 'X')) {
+            result = true;
+        }
         return result;
     }
 }
--- chapter_001/src/test/java/ru/job4j/array/MatrixTest.java
+++ chapter_001/src/test/java/ru/job4j/array/MatrixTest.java
@@ -1,8 +1,6 @@
 package ru.job4j.array;
 
 import org.junit.Test;
-
-import static org.hamcrest.Matchers.emptyIterable;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
