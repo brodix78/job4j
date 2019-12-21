6708132bb3ab62b0a9cce7724ffe692246dc4135
Ilya Broodnikov <brodnikov1950@gmail.com>
2019-12-21 12:44:33 +0400

    Слово заканчивается с ... #210129

--- chapter_001/src/main/java/ru/job4j/array/EndsWith.java
+++ chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -2,11 +2,13 @@ package ru.job4j.array;
 
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
+        boolean rsl = true;
         for (int i = 1; i <= post.length; i++) {
             if (word[word.length - i] != post[post.length - i]) {
-                return false;
+                rsl = false;
+                break;
             }
         }
-        return true;
+        return rsl;
     }
 }
