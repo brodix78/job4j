e8f460acd9744f5076297b88ee9da07c837df28f
Ilya Broodnikov <brodnikov1950@gmail.com>
2019-12-24 20:31:50 +0400

    Взаимодействие объектов.[#210266]

---
+++ chapter_002/src/main/java/ru/job4j/oop/BallStory.java
@@ -0,0 +1,15 @@
+package ru.job4j.oop;
+
+public class BallStory {
+    public static void main (String[] args) {
+        Ball ball = new Ball();
+        Hare hare = new Hare();
+        Wolf wolf = new Wolf();
+        Fox fox = new Fox();
+        hare.tryEat(ball);
+        ball.tryEat(ball);
+        wolf.tryEat(ball);
+        ball.tryEat(ball);
+        fox.tryEat(ball);
+    }
+}
