b455ec8eccda9ffdbc2459d65518545603eb5049
Ilya Broodnikov <brodnikov1950@gmail.com>
2019-12-27 17:39:52 +0400

    Аннотация @Override[#210276]

--- chapter_002/src/main/java/ru/job4j/inheritance/HtmlReport.java
+++ chapter_002/src/main/java/ru/job4j/inheritance/HtmlReport.java
@@ -1,6 +1,8 @@
 package ru.job4j.inheritance;
 
 public class HtmlReport extends TextReport {
+
+    @Override
     public String generate(String name, String body) {
         return "<hl>" + name + "</hl>" + "<br/><span>" +
                 body + "</span";
