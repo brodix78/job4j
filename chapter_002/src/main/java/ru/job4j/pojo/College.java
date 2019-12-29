bd4db959457e1c5ee8b98561d3acce93c1d44134
Ilya Broodnikov <brodnikov1950@gmail.com>
2019-12-29 13:12:54 +0400

    Модель данных.[#210280]

---
+++ chapter_002/src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,20 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class College {
+
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("Иванов Иван Иванович");
+        student.setGroup("1AC");
+        Calendar calendar = new GregorianCalendar(2019, 8 , 2);
+        Date date = calendar.getTime();
+        student.setDateIn(date);
+        System.out.println("Ф.И.О.: " + student.getName());
+        System.out.println("Группа: " + student.getGroup());
+        System.out.println("Дата принятия: " + student.getDateIn());
+    }
+}
