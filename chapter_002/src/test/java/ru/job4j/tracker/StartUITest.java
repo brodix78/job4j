e5f8fe0ea3bbded2ea0c957a366f7abe78f0d93f
Ilya Broodnikov <brodnikov1950@gmail.com>
2020-01-02 12:21:45 +0400

    Тестирование. Подготовка данных.[#210246]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,6 +1,9 @@
 package ru.job4j.tracker;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.core.Is.is;
 
@@ -18,5 +21,29 @@ public class StartUITest {
         for (int i = 0; i < answers.length; i++) {
             assertThat(rsl[i].getName(), is (answers[i]));
         }
+    }
+
+   @Test
+   public void replaceTest() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("First");
+        tracker.add(item);
+        String[] answers = {item.getId(), "Second"};
+        StartUI.replace(new StubInput(answers), tracker);
+        Item replaced = tracker.findById(item.getId());
+        assertThat(replaced.getName(), is ("Second"));
+   }
+
+   @Test
+    public void deleteTest() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item ("One");
+        Item item2 = new Item ("Two");
+        tracker.add(item1);
+        tracker.add(item2);
+        String[] answers = {item1.getId()};
+        StartUI.delete(new StubInput(answers),tracker);
+        assertNull(tracker.findById(answers[0]));
+        assertThat(tracker.findAll()[0], is(item2));
     }
 }
