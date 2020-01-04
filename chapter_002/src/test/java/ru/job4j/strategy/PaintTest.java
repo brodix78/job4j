fff4d48db0a797ffd8962e5f3f29b05e0b0add5a
Ilya Brodnikov <brodnikov1950@gmail.com>
2020-01-04 22:40:43 +0400

    Рефакторинг теста @Before @After[#210235]

--- chapter_002/src/test/java/ru/job4j/strategy/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/strategy/PaintTest.java
@@ -1,5 +1,7 @@
 package ru.job4j.strategy;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -7,14 +9,24 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 public class PaintTest {
+    private final PrintStream stdout = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void returnOutput() {
+        System.setOut(this.stdout);
+    }
+
     @Test
-    public void paintTest() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
+    public void paintTestWhenSquare() {
         new Paint().draw(new Square());
         assertThat(
-                new String(out.toByteArray()),
+                new String(this.out.toByteArray()),
                 is(new StringBuilder()
                         .append("+ + + + + +" + System.lineSeparator())
                         .append("+         +" + System.lineSeparator())
@@ -26,7 +38,25 @@ public class PaintTest {
                         .toString()
                 )
         );
-        System.setOut(stdout);
     }
+
+    @Test
+    public void paintTestWhenTriangle() {
+        new Paint().draw(new Triangle());
+        assertThat(
+                new String(this.out.toByteArray()),
+                is(new StringBuilder()
+                        .append("     +" + System.lineSeparator())
+                        .append("    + +" + System.lineSeparator())
+                        .append("   +   +" + System.lineSeparator())
+                        .append("  +     +" + System.lineSeparator())
+                        .append(" +       +" + System.lineSeparator())
+                        .append("+ + + + + +" + System.lineSeparator())
+                        .append(System.lineSeparator())
+                        .toString()
+                )
+        );
+    }
+
 
 }
