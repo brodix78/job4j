52d08a472d50e9c6d14eacd011b5591e6ccf43d6
Ilya Brodnikov <brodnikov1950@gmail.com>
2020-01-11 14:42:12 +0400

    Телефонный справочник на базе ArrayList [#41598]

---
+++ chapter_003/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -0,0 +1,26 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+
+public class PhoneDictionary {
+    private ArrayList<Person> persons = new ArrayList<>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    public ArrayList<Person> find(String key) {
+        ArrayList<Person> find = new ArrayList<>();
+        for (Person someone : this.persons) {
+            if (checkContain(someone, key)) {
+                find.add(someone);
+            }
+        }
+        return find;
+    }
+
+    private boolean checkContain(Person person, String key) {
+        return person.getName().contains(key) || person.getSurname().contains(key) ||
+                person.getPhone().contains(key) || person.getAddress().contains(key);
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -0,0 +1,18 @@
+package ru.job4j.search;
+
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+import java.util.ArrayList;
+
+public class PhoneDictionaryTest {
+    @Test
+    public void whenFindName() {
+        PhoneDictionary dictionary = new PhoneDictionary();
+        dictionary.add(new Person("Michael", "Jackson", "12345678", "Neverland"));
+        dictionary.add(new Person("Ivan", "Ivanov", "12345678", "Ivanovo"));
+        ArrayList<Person> find = dictionary.find("acks");
+        assertThat(find.get(0).getName(), is("Michael"));
+        assertThat(find.size(), is(1));
+    }
+}
