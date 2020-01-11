199e9b7856186a69880938caa3603b27a7e35be2
Ilya Brodnikov <brodnikov1950@gmail.com>
2020-01-11 21:32:56 +0400

    Идентичные тексты. [#212655]

---
+++ chapter_003/src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,22 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+import java.util.SplittableRandom;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String testText) {
+        HashSet<String> originWords = new HashSet<>();
+        boolean equal = true;
+        String[] original = originText.split(" ");
+        for (String word : original) {
+            originWords.add(word);
+        }
+        for (String origWord : originWords) {
+            equal = testText.contains(origWord);
+            if (equal == false) {
+                break;
+            }
+        }
+        return equal;
+    }
+}
