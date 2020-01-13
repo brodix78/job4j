cc3b32f99460501ac08883aea83dd28557850e60
Ilya Brodnikov <brodnikov1950@gmail.com>
2020-01-13 20:53:20 +0400

    Организовать сортировку User [#10034]

---
+++ chapter_003/src/main/java/ru/job4j/collection/User.java
@@ -0,0 +1,35 @@
+package ru.job4j.collection;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+public class User implements Comparable<User> {
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || this.getClass() != obj.getClass()) return false;
+        User user = (User) obj;
+        return this.age == user.age && this.name.equals(user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    @Override
+    public int compareTo(@NotNull User obj) {
+        int rsl = this.name.compareTo(obj.name);
+        if (rsl == 0) rsl = Integer.compare(this.age, obj.age);
+        return rsl;
+    }
+}
