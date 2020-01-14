523659969f70359cef5ff9494565975745dfbfba
Ilya Brodnikov <brodnikov1950@gmail.com>
2020-01-14 10:46:18 +0400

    Компаратор для строк.[#210201]

---
+++ chapter_003/src/main/java/ru/job4j/collection/StringCompare.java
@@ -0,0 +1,18 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class StringCompare implements Comparator<String> {
+    @Override
+    public int compare(String string1, String string2) {
+        int l1 = string1.length();
+        int l2 = string2.length();
+        int min = l1 < l2 ? l1 : l2;
+        int rsl = 0;
+        for (int i = 0; i < min; i++) {
+            rsl = Character.compare(string1.charAt(i), string2.charAt(i));
+            if (rsl != 0) return rsl;
+        }
+        return Integer.compare(l1, l2);
+    }
+}
