f575d807439111255715c99b852f85a909192ecf
Ilya Brodnikov <brodnikov1950@gmail.com>
2020-01-30 09:55:15 +0400

    Создать convert(Iterator<Iterator>)[#227207] упрощение

--- chapter_101/src/main/java/ru/job4j/iterator/Converter.java
+++ chapter_101/src/main/java/ru/job4j/iterator/Converter.java
@@ -11,17 +11,21 @@ public class Converter<Integer> implements Iterator {
 
     public Converter(Iterator<Iterator<Integer>> iterators) {
         this.iterators = iterators;
+        /* Здесь меня просите поставить проверку hasNext - зачем ?
+        *  любой hasNext, как и последующий iterators.next()  выкидывают тут NullPointerException в
+        *  случае если пытаться обработать Iterator<Iterator<Integer>> = null, в противном случае
+        *  this.iterator = this.iterators.next() штатно отрабатывает и нет необходимости наворачивать
+        *  локальный hasNext.
+        *  Вот хоть убейте: не понимаю какой в нем (hasNext) здесь смысл?
+        */
+        this.iterator = this.iterators.next();
     }
 
     @Override
     public boolean hasNext() {
-        try {
-            while (!this.iterator.hasNext() && this.iterators.hasNext()) {
+        while (!this.iterator.hasNext() && this.iterators.hasNext()) {
                 this.iterator = this.iterators.next();
             }
-        } catch (NullPointerException e) {
-            this.iterator = this.iterators.next();
-        }
         return this.iterator.hasNext();
     }
 
