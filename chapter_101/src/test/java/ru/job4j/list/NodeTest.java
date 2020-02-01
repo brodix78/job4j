0e5190c3f616d971481798748c85422552d52458
Ilya Brodnikov <brodnikov1950@gmail.com>
2020-02-01 23:08:08 +0400

    Мелкие доработки старого

--- chapter_101/src/test/java/ru/job4j/list/NodeTest.java
+++ chapter_101/src/test/java/ru/job4j/list/NodeTest.java
@@ -7,55 +7,5 @@ import static org.junit.Assert.assertThat;
 
 public class NodeTest {
 
-    @Test
-    public void whenHaveNoLoop() {
-        Node<Integer> first = new Node<>(1);
-        Node<Integer> two = new Node<>(2);
-        Node<Integer> third = new Node<>(3);
-        Node<Integer> four = new Node<>(4);
-        first.next = two;
-        two.next = third;
-        third.next = four;
-        four.next = null;
-        assertThat(first.hasCycle(first), is(false));
-    }
 
-    @Test
-    public void whenHaveLoopFourToOne() {
-        Node<Integer> first = new Node<>(1);
-        Node<Integer> two = new Node<>(2);
-        Node<Integer> third = new Node<>(3);
-        Node<Integer> four = new Node<>(4);
-        first.next = two;
-        two.next = third;
-        third.next = four;
-        four.next = first;
-        assertThat(first.hasCycle(first), is(true));
-    }
-
-    @Test
-    public void whenHaveLoopThreeToTwo() {
-        Node<Integer> first = new Node<>(1);
-        Node<Integer> two = new Node<>(2);
-        Node<Integer> third = new Node<>(3);
-        Node<Integer> four = new Node<>(4);
-        first.next = two;
-        two.next = third;
-        third.next = two;
-        four.next = null;
-        assertThat(first.hasCycle(first), is(true));
-    }
-
-    @Test
-    public void whenHaveLoopOneToOne() {
-        Node<Integer> first = new Node<>(1);
-        Node<Integer> two = new Node<>(2);
-        Node<Integer> third = new Node<>(3);
-        Node<Integer> four = new Node<>(4);
-        first.next = first;
-        two.next = third;
-        third.next = four;
-        four.next = null;
-        assertThat(first.hasCycle(first), is(true));
-    }
 }
