3146a245760efc05dd223d2e94e3e117a02defb5
Ilya Brodnikov <brodnikov1950@gmail.com>
2020-02-04 11:16:57 +0400

    Очередь на двух стеках[#227202]

--- chapter_101/src/main/java/ru/job4j/list/SimpleQueue.java
+++ chapter_101/src/main/java/ru/job4j/list/SimpleQueue.java
@@ -3,42 +3,38 @@ package ru.job4j.list;
 import java.util.NoSuchElementException;
 
 public class SimpleQueue<E> {
-    private Node<E> last;
 
-    private static class Node<E> {
-        E data;
-        Node<E> previous;
-        Node(E data) {
-            this.data = data;
-        }
-    }
+    private SimpleStack<E> in = new SimpleStack<>();
+    private SimpleStack<E> out = new SimpleStack<>();
+
 
     public boolean push(E data) {
         boolean rsl = false;
         if (data != null) {
-            Node<E> node = new Node<>(data);
-            rsl = true;
-            node.previous = this.last;
-            this.last = node;
+            rsl = in.push(data);
         }
         return rsl;
     }
 
     public E poll() {
-        Node<E> node = this.last;
         E rsl;
-        if (this.last == null)  {
-            throw new NoSuchElementException("Stack is empty");
-        }
-        if (node.previous != null) {
-            while (node.previous.previous != null) {
-                node = node.previous;
-            }
-            rsl = node.previous.data;
-            node.previous = null;
-        } else {
-            rsl = node.data;
-            this.last = null;
+        try {
+            rsl = out.poll();
+        } catch (NoSuchElementException ex) {
+            boolean inEmpty = true;
+            do {
+                try {
+                    out.push(in.poll());
+                    inEmpty = false;
+                } catch (NoSuchElementException e) {
+                    if (inEmpty) {
+                        throw new NoSuchElementException("Queue is empty");
+                    } else {
+                        inEmpty = true;
+                    }
+                }
+            } while (!inEmpty);
+            rsl = out.poll();
         }
         return rsl;
     }
--- chapter_101/src/test/java/ru/job4j/list/SimpleQueueTest.java
+++ chapter_101/src/test/java/ru/job4j/list/SimpleQueueTest.java
@@ -14,8 +14,8 @@ public class SimpleQueueTest {
         list.push(1);
         list.push(2);
         list.push(3);
-        list.push(4);
         assertThat(list.poll(), is(1));
+        list.push(4);
         assertThat(list.poll(), is(2));
         assertThat(list.poll(), is(3));
         assertThat(list.poll(), is(4));
