08dee9c6512b7c0ebed910e4548d5158cc7e57da
Ilya Brodnikov <brodnikov1950@gmail.com>
2020-02-05 17:29:23 +0400

    Создать контейнер на базе связанного списка [#227201]

--- chapter_101/src/main/java/ru/job4j/list/ReferenceList.java
+++ chapter_101/src/main/java/ru/job4j/list/ReferenceList.java
@@ -2,6 +2,7 @@ package ru.job4j.list;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
@@ -9,6 +10,7 @@ public class ReferenceList<E> implements Iterable<E> {
     private Node<E> first;
     private Node<E> last;
     private int length;
+    private int modCount;
 
     private static class Node<E> {
         E data;
@@ -32,6 +34,7 @@ public class ReferenceList<E> implements Iterable<E> {
             }
             this.last = node;
             length++;
+            modCount++;
         }
         return rsl;
     }
@@ -59,9 +62,35 @@ public class ReferenceList<E> implements Iterable<E> {
         return length;
     }
 
+    private class ReferenceListIterator<E> implements Iterator<E> {
+        private int expectedModCount;
+        private Node<E> node = new Node<>(null);
+
+        public ReferenceListIterator() {
+            this.expectedModCount = modCount;
+            this.node.next = (Node<E>) first;
+        }
+
+        @Override
+        public boolean hasNext() {
+            if (this.expectedModCount != modCount) {
+                throw new ConcurrentModificationException("List is changed");
+            }
+            return node != null && node.next != null;
+        }
+
+        @Override
+        public E next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException("End of List is reached");
+            }
+            this.node = this.node.next;
+            return this.node.data;
+        }
+    }
     @NotNull
     @Override
     public Iterator<E> iterator() {
-        return new ReferenceListIterator<>(this);
+        return new ReferenceListIterator<>();
     }
 }
