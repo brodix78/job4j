15266610a1a78cf96a34913b25c1263e729ab0e9
Ilya Brodnikov <brodnikov1950@gmail.com>
2020-02-09 09:30:43 +0400

    Создать элементарную структуру дерева[#227229]

---
+++ chapter_101/src/test/java/ru/job4j/tree/OwnTreeTest.java
@@ -0,0 +1,73 @@
+package ru.job4j.tree;
+
+import org.junit.Test;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class OwnTreeTest {
+
+    @Test
+public void when6ElFindLastThen6() {
+    SimpleTree<Integer> tree = new OwnTree<>(1);
+    tree.add(1, 2);
+    tree.add(1, 3);
+    tree.add(1, 4);
+    tree.add(4, 5);
+    tree.add(5, 6);
+    assertThat(
+            tree.findBy(2).isPresent(),
+            is(true)
+    );
+}
+
+    @Test
+    public void when6ElFindNotExitThenOptionEmpty() {
+        SimpleTree<Integer> tree = new OwnTree<>(1);
+        tree.add(1, 2);
+        assertThat(
+                tree.findBy(7).isPresent(),
+                is(false)
+        );
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void iteratorTest() {
+        SimpleTree<Integer> tree = new OwnTree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        tree.add(4, 5);
+        tree.add(5, 6);
+        Iterator<Integer> it = tree.iterator();
+        int checkSum = 0;
+        while (it.hasNext()) {
+            checkSum = checkSum + it.next();
+        }
+        assertThat(checkSum, is(1 + 2 + 3 + 4 + 5 + 6));
+        it.next();
+    }
+
+    @Test(expected = ConcurrentModificationException.class)
+    public void iteratorCheckModificationsCountAndNotAddingExistingElements() {
+        SimpleTree<Integer> tree = new OwnTree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        tree.add(4, 5);
+        tree.add(5, 6);
+        Iterator<Integer> it = tree.iterator();
+        int checkSum = 0;
+        while (it.hasNext()) {
+            checkSum = checkSum + it.next();
+        }
+        tree.add(4, 5);
+        assertThat(it.hasNext(), is(false));
+        tree.add(4, 7);
+        it.hasNext();
+    }
+}
