b3b17505274ecda327da89053949c6f38720918a
Ilya Brodnikov <brodnikov1950@gmail.com>
2020-02-17 22:18:24 +0400

    промежуточная

--- chapter_101/src/main/java/ru/job4j/exam/AnalizeMap.java
+++ chapter_101/src/main/java/ru/job4j/exam/AnalizeMap.java
@@ -3,12 +3,15 @@ package ru.job4j.exam;
 import java.util.HashMap;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class AnalizeMap {
 
     public Info diff(List<User> previous, List<User> current) {
         int changed = 0;
         int deleted = 0;
+      //  HashMap<Integer, String> cur = new HashMap<>(
+      //          current.stream().collect(Collectors.toMap(i -> i.id, i -> i.name)));
         HashMap<Integer, String> cur = new HashMap<>();
         for (User user : current) {
             cur.put(user.id, user.name);
