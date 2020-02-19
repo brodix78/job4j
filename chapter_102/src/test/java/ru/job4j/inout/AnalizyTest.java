035d9b5810694cd8358f7bfce6949e65f2e3628e
Ilya Brodnikov <brodnikov1950@gmail.com>
2020-02-19 22:14:05 +0400

    мелочь

--- chapter_102/src/test/java/ru/job4j/inout/AnalizyTest.java
+++ chapter_102/src/test/java/ru/job4j/inout/AnalizyTest.java
@@ -1,7 +1,9 @@
 package ru.job4j.inout;
 
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.*;
 
@@ -9,12 +11,16 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class AnalizyTest {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
 
     @Test
-    public void simplyTesting() {
-        String log = "./src/main/java/ru/job4j/inout/temp/sys.log";
-        String unv = "./src/main/java/ru/job4j/inout/temp/unavailable.csv";
-        try (PrintWriter out = new PrintWriter(new FileOutputStream(log))) {
+    public void simplyTesting() throws IOException {
+        String log = "sys.log";
+        String unv = "unavailable.csv";
+        File logFile = folder.newFile(log);
+        File unvFile = folder.newFile(unv);
+        try (PrintWriter out = new PrintWriter(logFile)) {
             out.println("200 10:56:01");
             out.println("500 10:57:01");
             out.println("400 10:58:01");
@@ -25,8 +31,8 @@ public class AnalizyTest {
             e.printStackTrace();
         }
         Analizy analizy = new Analizy();
-        analizy.unavailable(log, unv);
-        try (BufferedReader in = new BufferedReader(new FileReader(unv))) {
+        analizy.unavailable(logFile.getAbsolutePath(), unvFile.getAbsolutePath());
+        try (BufferedReader in = new BufferedReader(new FileReader(unvFile))) {
             assertThat(in.readLine(), is("10:57:01;10:59:01"));
             assertThat(in.readLine(), is("11:01:02;11:02:02"));
             Assert.assertNull(in.readLine());
