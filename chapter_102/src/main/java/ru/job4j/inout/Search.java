a21f2bf6a336ffe64610d509e4d60d58d685b363
Ilya Brodnikov <brodnikov1950@gmail.com>
2020-02-21 19:32:18 +0400

    Сканирование файловой системы.[#227234]

---
+++ chapter_102/src/main/java/ru/job4j/inout/Search.java
@@ -0,0 +1,26 @@
+package ru.job4j.inout;
+
+import java.io.File;
+import java.util.*;
+
+public class Search {
+
+    public List<File> files(String parent, List<String> exts) {
+        File path = new File(parent);
+        HashSet<String> filesExt = new HashSet<>(exts);
+        LinkedList<File> directories = new LinkedList<>(List.of(path));
+        List<File> output = new ArrayList<>();
+        while (!directories.isEmpty()) {
+            Arrays.stream(directories.pop().listFiles())
+                    .forEach(file -> {
+                        if (file.isDirectory()) {
+                            directories.push(file);
+                        } else if (filesExt.contains(file.getName()
+                                .substring(file.getName().lastIndexOf(".") + 1))) {
+                            output.add(file);
+                        }
+                    });
+        }
+        return output;
+    }
+}
