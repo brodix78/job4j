4341e146c1750d40b391631446e37601b9df0d66
Ilya Brodnikov <brodnikov1950@gmail.com>
2020-02-21 20:55:38 +0400

    Тестирование IO[#227235]

--- chapter_102/src/test/java/ru/job4j/inout/AnalizyTest.java
+++ chapter_102/src/test/java/ru/job4j/inout/AnalizyTest.java
@@ -1,44 +1,49 @@
 package ru.job4j.inout;
 
-import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
-
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class AnalizyTest {
+
     @Rule
     public TemporaryFolder folder = new TemporaryFolder();
 
-    @Test
-    public void simplyTesting() throws IOException {
-        String log = "sys.log";
-        String unv = "unavailable.csv";
-        File logFile = folder.newFile(log);
-        File unvFile = folder.newFile(unv);
-        try (PrintWriter out = new PrintWriter(logFile)) {
-            out.println("200 10:56:01");
-            out.println("500 10:57:01");
-            out.println("400 10:58:01");
-            out.println("200 10:59:01");
-            out.println("500 11:01:02");
-            out.println("200 11:02:02");
+    public ArrayList<String> outData(String[] inData) throws IOException {
+        File logFile = folder.newFile("sys.log");
+        File unvFile = folder.newFile("unavailable.csv");
+        Analizy analizy = new Analizy();
+        ArrayList<String> outData = new ArrayList<>();
+        try (PrintWriter out = new PrintWriter(logFile);
+             BufferedReader in = new BufferedReader(new FileReader(unvFile))) {
+            for (String line:inData) {
+                out.println(line);
+            }
+            out.close();
+            analizy.unavailable(logFile.getPath(), unvFile.getPath());
+            String line;
+            while ((line = in.readLine()) != null) {
+                outData.add(line);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        Analizy analizy = new Analizy();
-        analizy.unavailable(logFile.getAbsolutePath(), unvFile.getAbsolutePath());
-        try (BufferedReader in = new BufferedReader(new FileReader(unvFile))) {
-            assertThat(in.readLine(), is("10:57:01;10:59:01"));
-            assertThat(in.readLine(), is("11:01:02;11:02:02"));
-            Assert.assertNull(in.readLine());
-        } catch (Exception om) {
-            om.printStackTrace();
-        }
+        return outData;
+    }
+
+    @Test
+    public void simplyTesting() throws IOException {
+        List<String> rsl = outData(new String[] {"200 10:56:01", "500 10:57:01",
+                "400 10:58:01", "200 10:59:01", "500 11:01:02", "200 11:02:02"});
+        assertThat(rsl.get(0), is("10:57:01;10:59:01"));
+        assertThat(rsl.get(1), is("11:01:02;11:02:02"));
+        assertThat(rsl.size(), is (2));
     }
 }
 
