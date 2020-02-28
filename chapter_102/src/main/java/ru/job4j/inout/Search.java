b3b2a36b66253530bec757f44f896744b96cccc5
Ilya Brodnikov <brodnikov1950@gmail.com>
2020-02-28 11:26:23 +0400

    Сканирование файловой системы.[#227234]

--- chapter_102/src/main/java/ru/job4j/inout/Search.java
+++ chapter_102/src/main/java/ru/job4j/inout/Search.java
@@ -2,21 +2,35 @@ package ru.job4j.inout;
 
 import java.io.File;
 import java.util.*;
+import java.util.function.Predicate;
 
 public class Search {
 
-    public List<File> files(String parent, List<String> exts) {
+    private HashSet<String> filesExl;
+
+    public List<File> files(String parent, Predicate<File> search) {
         File path = new File(parent);
-        HashSet<String> filesExt = new HashSet<>(exts);
         List<File> output = new ArrayList<>();
-        for (File file : path.listFiles()) {
+        for (File file : Objects.requireNonNull(path.listFiles())) {
             if (file.isDirectory()) {
-                output.addAll(files(file.toString(), exts));
-            } else if (filesExt.contains(file.getName()
-                    .substring(file.getName().lastIndexOf(".") + 1))) {
+                output.addAll(files(file.toString(), search));
+            } else if (search.test(file)) {
                 output.add(file);
             }
         }
         return output;
+    }
+
+    private Predicate<File> searchByTypeExclude = new Predicate<>() {
+        @Override
+        public boolean test(File file) {
+            return filesExl.contains(file.getName()
+                    .substring(file.getName().lastIndexOf(".") + 1));
+        }
+    };
+
+    public List<File> filesByTypeExclude(String parent, List<String> excl) {
+        this.filesExl = new HashSet<>(excl);
+        return files(parent, searchByTypeExclude);
     }
 }
--- chapter_102/src/test/java/ru/job4j/inout/SearchTest.java
+++ chapter_102/src/test/java/ru/job4j/inout/SearchTest.java
@@ -13,7 +13,7 @@ public class SearchTest {
     public void myFolderTesting() {
         String path = ".";
         Search search = new Search();
-        List<File> rsl = search.files(path, List.of("java"));
+        List<File> rsl = search.filesByTypeExclude(path, List.of("java"));
         assertThat(rsl.contains(new File("./src/main/java/ru/job4j/inout/Abuse.java")), is(true));
         assertThat(rsl.contains(new File("./src/main/java/ru/job4j/inout/Config.java")), is(true));
         assertThat(rsl.contains(new File("./src/main/java/ru/job4j/inout/app.prop")), is(false));
