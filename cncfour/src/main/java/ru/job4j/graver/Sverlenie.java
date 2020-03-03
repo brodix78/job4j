a3fac02288ee241703118772b3e216137dc3d379
Ilya Brodnikov <brodnikov1950@gmail.com>
2020-03-03 19:03:22 +0400

    mein

--- cncfour/src/main/java/ru/job4j/graver/Sverlenie.java
+++ cncfour/src/main/java/ru/job4j/graver/Sverlenie.java
@@ -2,40 +2,86 @@ package ru.job4j.graver;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.Arrays;
+import java.util.HashMap;
 
 public class Sverlenie extends Operation{
 
     private String fileOutput;
     private String spindle;
-    private int turns;
+    private float turns;
     private float feed;
-    private float Start;
-    private float Finish;
+    private float workDeep;
+    private float start;
+    private float deep;
     private float safeStart;
-    private ArrayList<Float> points;
+    private ArrayList<Float[]> points;
+    /*  Последовательно значения в массиве:
+    /   Четные элементы (начиная с 0)
+    /     0. Координата безопасного подхода-отхода по оси сверления;
+    /     1. Координата начала сверления;
+    /     2. Координата начала сверления;
+    /     3. Обороты шпинделя;
+    /     4. Подача мм/оборт;
+    /     5. Подача мм на один заход;
+    /   Нечетные элементы:
+    /     координата 1, координата 2, координата 3 (если используется):
+    /       Для шпинделя 3,4 не указывается в массиве торчит один 0;
+    /       Для шпинделя 1g пара X, Y, C3 (горизонтальный приводной инструмент в верхней башке)
+    /       Для шпинделя 2g пара X, C4 (горизонтальный приводной инструмент в нижней башке)
+    /       Для шпинделя 1v пара Y, Z, C3 (вертикальный приводной инструмент в верхней башке)
+    /       Для шпинделя 2v пара Z, C4 (вертикальный приводной инструмент в нижней башке)
+   */
+    String[] values = new String[]{"one", "two", "three", "four", "five", "six", "seven", "eight"};
+
+    private String lines(String in, float[] attr) {
+        int index = 1;
+        for (Float att:attr) {
+            in = in.replace(values[index++], String.format("%.3f",att));
+        }
+        return in;
+    }
+
+    private void setWorkValues(int index) {
+        safeStart = points.get(index)[0];
+        start = points.get(index)[1];
+        deep = points.get(index)[2];
+        turns = points.get(index)[3];
+        feed = points.get(index)[4];
+        workDeep = points.get(index)[5];
+    }
+
+    private HashMap<String, ArrayList<String>> libSp() {
+        /*  0. Обороты шпинделя
+        /   1. Подача
+        /   2. Безопасный первый подход
+        /   3. Последующий подход
+        /   4. Сверление
+         */
+        HashMap<String, ArrayList<String>> lib = new HashMap<>();
+        try(BufferedReader in = new BufferedReader(new FileReader(new File("./in/Sverlenie/data.rul")))) {
+            String line;
+            while ((line = in.readLine()) != null){
+                ArrayList<String> data = new ArrayList(Arrays.asList(line.split("@@")));
+                String key = data.get(0);
+                data.remove(0);
+                lib.put(key, data);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return lib;
+    }
 
     protected void proG() {
         spindle = spindle.toLowerCase();
         try (BufferedWriter nc = new BufferedWriter(new FileWriter(new File(fileOutput)))){
+            setWorkValues(0);
             StringBuilder rsl = new StringBuilder();
-            if ("4".equals(spindle)) {
-                rsl.append(String.format("G0 Z%.3f%n G0 X0.%n", safeStart));
-            } else if ("3".equals(spindle)) {
-                rsl.append(String.format("G0 X0. Z%.3f%n", safeStart));
-            } else if ("1g".equals(spindle)) {
-                rsl.append(String.format("G0 X%.3f Y%.3f Z%.3f%n", points.get(0), points.get(1), safeStart));
-            } else if ("1v".equals(spindle)) {
-                rsl.append(String.format("G0 X%.3f Y%.3f Z-10.%n", safeStart, points.get(0)));
-            } else if ("2g".equals(spindle)) {
-                rsl.append(String.format("G0 Z%.3f%n G0 X%.3f%n", safeStart, points.get(0)));
-            }else if ("2v".equals(spindle)) {
-                rsl.append(String.format("G0 Z%.3f%n G0 X%.3f%n", points.get(0), safeStart));
-            }
             rsl.append(generator());
-            String[] fill = fillFromFile("SverSp" + spindle +".ncst");
+            String[] fill = fillFromFile("/Sverlenie/Sp" + spindle +".ncst");
             nc.write(fill[0]);
-
+            nc.write(rsl.toString());
             nc.write(fill[1]);
         } catch (Exception e) {
             e.printStackTrace();
@@ -43,35 +89,25 @@ public class Sverlenie extends Operation{
     }
 
     private String generator() {
-        Iterator pointsIt = points.iterator();
-        while (pointsIt.hasNext()) {
-            StringBuilder rsl = new StringBuilder();
-            if ("4".equals(spindle)) {
-                rsl.append("G0 Z2.%n G0 X0.%n");
-                rsl.append("G0 Z2.%n");
+        StringBuilder rsl = new StringBuilder();
+        HashMap<String, ArrayList<String>> lib = libSp();
+        int index = 0;
+        while (index < points.size()) {
+            Float[] line = points.get(index);
+            if (line[3] != turns) {
+                rsl.append(String.format(lines(lib.get(spindle).get(0), new float[]{line[3]})));
             }
-            if ("3".equals(spindle)) {
-                rsl.append("G0 X0. Z-2.%n");
-                rsl.append("G0 Z-2.%n");
+            if (line[4] != feed) {
+                rsl.append(String.format(lines(lib.get(spindle).get(1), new float[]{line[4]})));
             }
-            if ("1g".equals(spindle)) {
-                rsl.append("G0 X0. Z-2.%n");
-                rsl.append("G0 Z-2.%n");
-            }
-            if ("1v".equals(spindle)) {
-                rsl.append("G0 X0. Z-2.%n");
-                rsl.append("G0 Z-2.%n");
-            }
-            if ("1g".equals(spindle)) {
-                rsl.append("G0 X0. Z-2.%n");
-                rsl.append("G0 Z-2.%n");
-            }
-
-            if ("1v".equals(spindle)) {
-                rsl.append("G0 X0. Z-2.%n");
-                rsl.append("G0 Z-2.%n");
-            }
+            setWorkValues(index++);
+            int type = index == 1 ? 2 : 3;
+            line = points.get(index++);
+            rsl.append(String.format(lines(lib.get(spindle).get(type), new float[]{
+                        safeStart, line[0], line[1], line[2]})));
+            rsl.append(String.format(lines(lib.get(spindle).get(3), new float[]{
+                    safeStart, start, deep, workDeep * 2, workDeep })));
         }
-        return null;
+        return rsl.toString();
     }
 }
