93871b3ba57fb41aae1aa7b509a28616941c4169
Ilya Brodnikov <brodnikov1950@gmail.com>
2020-03-06 18:03:58 +0400

    mein

--- cncfour/src/main/java/ru/job4j/graver/Sverlenie.java
+++ cncfour/src/main/java/ru/job4j/graver/Sverlenie.java
@@ -16,35 +16,37 @@ public class Sverlenie extends Operation{
     private float start;
     private float deep;
     private float safeStart;
-    ArrayList<String> data;
-    private ArrayList<Float[]> points = new ArrayList<>();
+    private ArrayList<String> data;
+    private ArrayList<Float[]> workSets = new ArrayList<>();
+    LinkedHashMap<String, Float> workVariables = new LinkedHashMap<>();
+
 
     public Sverlenie(String spindle, ArrayList<String> data){
         this.spindle = spindle;
         this.data = data;
     }
 
-    String[] values = new String[]{"one", "two", "three", "four", "five", "six", "seven", "eight"};
+    protected class Rule {
+        String text;
+        String marker;
 
-    private String lines(String in, float[] attr) {
-        int index = 1;
-        for (Float att:attr) {
-            in = in.replace(values[index++], String.format("%.3f",att));
+        private Rule(String text, String marker) {
+            this.text = text;
+            this.marker = marker;
+        }
+    }
+
+    private String lines(String in, Float[] workSet) {
+        int index = 0;
+        for (String variable:workVariables.keySet()) {
+            in = in.replace(workVariables.get(variable), String.format("%.3f", workSet[index]));
+            index++;
         }
         return in;
     }
 
-    private void setWorkValues(int index) {
-        safeStart = points.get(index)[0];
-        start = points.get(index)[1];
-        deep = points.get(index)[2];
-        turns = points.get(index)[3];
-        feed = points.get(index)[4];
-        workDeep = points.get(index)[5];
-    }
-
-    private ArrayList<String> rulesRead() {
-        ArrayList<String> rules = new ArrayList<>();
+    private ArrayList<Rule> rulesRead() {
+        ArrayList<Rule> rules = new ArrayList<>();
         int blockFind = 0;
         try(BufferedReader in = new BufferedReader(new FileReader(new File("./graver/in/Sverlenie/data.rul")))) {
             String line;
@@ -54,7 +56,11 @@ public class Sverlenie extends Operation{
                         blockFind = blockFind == 0 ? 1 : 2;
                     } else {
                         if (blockFind == 1) {
-                            rules.add(line);
+                            if (line.startsWith("v")) {  //чтение видов переменных
+                                pointsGen(line.substring(2));
+                            } else {
+                                rules.add(new Rule(line.substring(1), line.substring(0, line.indexOf(" "))));
+                            }
                         }
                     }
             }
@@ -64,38 +70,33 @@ public class Sverlenie extends Operation{
         return rules;
     }
 
-    private void pointsGen(String[] rules) {
-        ArrayList<LinkedHashMap<String, Float>> pointRules = new ArrayList<>();
-        for (String ruleLine:rules) {
-            LinkedHashMap<String, Float> rl = new LinkedHashMap<String, Float>();
-            Arrays.stream(ruleLine.split(" "))
+    private void pointsGen(String rule) {
+        Arrays.stream(rule.split(" "))
                     .forEach(line -> {
                         String[] pair = line.split("=");
-                        rl.put(pair[0], Float.parseFloat(pair[1].replace(",", ".")));
+                        workVariables.put(pair[0], Float.parseFloat(pair[1].replace(",", ".")));
                     });
-            pointRules.add(rl);
-        }
         for (String dataLine:data) {
             HashMap<String, Float> lineToMap = new HashMap<>();
             Arrays.stream(dataLine.split(" "))
+                    .filter(val -> !val.isEmpty() && val.contains("=")
+                            && !val.startsWith("=") && !val.endsWith("="))
                     .forEach(line -> {
                         String[] pair = line.split("=");
                         lineToMap.put(pair[0], Float.parseFloat(pair[1].replace(",", ".")));
                     });
-            for (LinkedHashMap<String, Float> dataForLine:pointRules) {
-                Float[] point = new Float[dataForLine.size()];
-                int index = 0;
-                for (String key:dataForLine.keySet()){
-                    point[index++] = lineToMap.containsKey(key) ? lineToMap.get(key) : dataForLine.get(key);
-                }
-                points.add(point);
+            Float[] oneSet = new Float[workVariables.size()];
+            int index = 0;
+            for (String key:workVariables.keySet()){
+                oneSet[index++] = lineToMap.containsKey(key) ? lineToMap.get(key) : workVariables.get(key);
             }
+            workSets.add(oneSet);
         }
     }
 
+
     protected String proG() {
         spindle = spindle.toLowerCase();
-        setWorkValues(0);
         StringBuilder rsl = new StringBuilder();
         String[] fill = fillFromFile("./graver/Sverlenie/Sp" + spindle +".ncst");
         rsl.append(fill[0]);
@@ -106,24 +107,31 @@ public class Sverlenie extends Operation{
 
     private String generator() {
         StringBuilder rsl = new StringBuilder();
-        ArrayList<String> rules = rulesRead();
-        pointsGen(new String[]{rules.get(5), rules.get(6)});
+        ArrayList<Rule> rules = rulesRead();
         int index = 0;
-        while (index < points.size()) {
-            Float[] coordinate = points.get(index);
-            if (coordinate[3] != turns) {
-                rsl.append(String.format(lines(rules.get(0), new float[]{coordinate[3]})));
+        Float[] prevoius = null;
+        for (Float[] workSet : workSets) {
+            index++;
+            for (Rule rule : rules) {
+                if ("r".equals(rule.marker)) {
+                    rsl.append(lines(rule.text, workSet));
+                } else if ("s".equals(rule.marker) && index > 1) {
+                    rsl.append(lines(rule.text, workSet));
+                } else if ("f".equals(rule.marker) && index == 1) {
+                    rsl.append(lines(rule.text, workSet));
+                } else if(rule.marker.startsWith("p")) {
+                    if (index == 1) {
+                        rsl.append(lines(rule.text, workSet));
+                    } else {
+                        ArrayList<String> wV = new ArrayList(workVariables.keySet());
+                        int pos = wV.indexOf(rule.marker.substring(1));
+                        if(workSet[pos] != prevoius[pos]) {
+                            rsl.append(lines(rule.text, workSet));
+                        }
+                    }
+                }
             }
-            if (coordinate[4] != feed) {
-                rsl.append(String.format(lines(rules.get(1), new float[]{coordinate[4]})));
-            }
-            setWorkValues(index++);
-            int type = index == 1 ? 2 : 3;
-            coordinate = points.get(index++);
-            rsl.append(String.format(lines(rules.get(type), new float[]{
-                        safeStart, coordinate[0], coordinate[1], coordinate[2]})));
-            rsl.append(String.format(lines(rules.get(4), new float[]{
-                    safeStart, start, deep, workDeep * 2, workDeep })));
+            prevoius = workSet;
         }
         return rsl.toString();
     }
