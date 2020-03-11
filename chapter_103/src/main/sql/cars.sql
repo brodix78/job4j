dc98cb896ef430148432bb6eff51abcd1530ccd7
Ilya Brodnikov <brodnikov1950@gmail.com>
2020-03-11 19:13:01 +0400

    Хранилище машин[#227190]

--- chapter_103/src/main/sql/cars.sql
+++ chapter_103/src/main/sql/cars.sql
@@ -1,16 +1,7 @@
-Нужно написать SQL скрипты:
-
 Создать структур данных в базе.
 Таблицы.
    Кузов. Двигатель, Коробка передач.
 Создать структуру Машина. Машина не может существовать без данных из п.1.
-Заполнить таблицы через insert.
-Создать SQL запросы:
-
-1. Вывести список всех машин и все привязанные к ним детали.
-
-2. Вывести отдельно детали, которые не используются в машине, кузова, двигатели, коробки передач.
-
 
 CREATE TABLE bodies (
     id SERIAL PRIMARY KEY,
@@ -32,32 +23,39 @@ CREATE TABLE cars (
     model VARCHAR(30),
     body_id INT REFERENCES bodies(id),
     engine_id INT REFERENCES engines(id),
-    transmission_id INT REFERENCES transmissions(id),
+    transmission_id INT REFERENCES transmissions(id)
 );
 
-INSERT INTO bodies(model, type) VALUES ("c52", "sedan");
-INSERT INTO bodies(model, type) VALUES ("c54", "coupe");
-INSERT INTO bodies(model, type) VALUES ("c60", "suv");
-INSERT INTO bodies(model, type) VALUES ("a72", "pickup");
+Заполнить таблицы через insert.
 
-INSERT INTO engines(model, type) VALUES ("S16I4", "petrol");
-INSERT INTO engines(model, type) VALUES ("S24I4", "petrol");
-INSERT INTO engines(model, type) VALUES ("S30V6", "petrol");
-INSERT INTO engines(model, type) VALUES ("D40V6", "diesel");
+INSERT INTO bodies(model, type) VALUES ('c52', 'sedan');
+INSERT INTO bodies(model, type) VALUES ('c54', 'coupe');
+INSERT INTO bodies(model, type) VALUES ('c60', 'suv');
+INSERT INTO bodies(model, type) VALUES ('a72', 'pickup');
 
-INSERT INTO transmissions(model, type) VALUES ("MT6V2", "manual");
-INSERT INTO transmissions(model, type) VALUES ("MT7SV3", "manual");
-INSERT INTO transmissions(model, type) VALUES ("AT6V1", "automatic");
-INSERT INTO transmissions(model, type) VALUES ("AT6FV2", "automatic");
+INSERT INTO engines(model, type) VALUES ('S16I4', 'petrol');
+INSERT INTO engines(model, type) VALUES ('S24I4', 'petrol');
+INSERT INTO engines(model, type) VALUES ('S30V6', 'petrol');
+INSERT INTO engines(model, type) VALUES ('D40V6', 'diesel');
 
-INSERT INTO cars(model, body_id, engine_id, transmission_id) VALUES ("Simple Sedan", 1, 1, 1);
-INSERT INTO cars(model, body_id, engine_id, transmission_id) VALUES ("Simple Coupe", 2, 2, 2);
-INSERT INTO cars(model, body_id, engine_id, transmission_id) VALUES ("Simple Sedan", 3, 3, 3);
+INSERT INTO transmissions(model, type) VALUES ('MT6V2', 'manual');
+INSERT INTO transmissions(model, type) VALUES ('MT7SV3', 'manual');
+INSERT INTO transmissions(model, type) VALUES ('AT6V1', 'automatic');
+INSERT INTO transmissions(model, type) VALUES ('AT6FV2', 'automatic');
+
+INSERT INTO cars(model, body_id, engine_id, transmission_id) VALUES ('Simple Sedan', 1, 1, 1);
+INSERT INTO cars(model, body_id, engine_id, transmission_id) VALUES ('Simple Coupe', 2, 2, 2);
+INSERT INTO cars(model, body_id, engine_id, transmission_id) VALUES ('Simple Sedan', 3, 3, 3);
+
+
+1. Вывести список всех машин и все привязанные к ним детали.
 
 SELECT c.id, c.model, b.model, e.model, t.model FROM cars AS c
-LEFT OUTER JOIN bodies AS b ON c.body_id = b.id,
-                engines AS e ON c.engine_id = e.id,
-                transmissions AS t ON t.transmissions = t.id;
+LEFT OUTER JOIN bodies AS b ON c.body_id = b.id
+LEFT OUTER JOIN engines AS e ON c.engine_id = e.id
+LEFT OUTER JOIN transmissions AS t ON c.transmission_id = t.id;
+
+2. Вывести отдельно детали, которые не используются в машине, кузова, двигатели, коробки передач.
 
 SELECT b.model FROM bodies AS b
 LEFT OUTER JOIN cars AS c ON b.id = c.body_id
