622ffb9bb93e605a3810fb977cc4ccccacea4a20
Ilya Brodnikov <brodnikov1950@gmail.com>
2020-03-31 18:15:21 +0400

    Парсер вакансий на sql.ru[#227180]

--- chapter_103/src/main/java/ru/job4j/exam/Vacancy.java
+++ chapter_103/src/main/java/ru/job4j/exam/Vacancy.java
@@ -1,5 +1,7 @@
 package ru.job4j.exam;
 
+import java.util.Objects;
+
 public class Vacancy {
     public String name;
     public String text;
@@ -9,5 +11,19 @@ public class Vacancy {
         this.name = head;
         this.text = text;
         this.link = link;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vacancy vacancy = (Vacancy) o;
+        return name.equals(vacancy.name) &&
+                text.equals(vacancy.text);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, text);
     }
 }
