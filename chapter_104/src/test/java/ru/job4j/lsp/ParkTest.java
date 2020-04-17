3f240de222bdb7b547764164bb269472b5abd66a
brodix78 <brodnikov1950@gmail.com>
2020-04-17 21:11:40 +0400

    Parking Test

---
+++ chapter_104/src/test/java/ru/job4j/lsp/ParkTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.lsp;
+
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.List;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ParkTest {
+
+    Parking parking = new Parking();
+    StringWriter out = new StringWriter();
+    PrintWriter output = new PrintWriter(out);
+
+    @Test
+    public void testMenu() {
+        BufferedReader input = new BufferedReader(new StringReader("3%n"));
+        Park park = new Park(parking, input, output, List.of(AddCar, GetCar));
+        String exp = String.format("1. Park a car;%n2. Leave parking;%n3. Exit.%n%nInput your choice (1-3): ");
+        output.flush();
+        assertThat(out.toString(), is(exp));
+    }
+}
