6681f2fadaa75da28b79e18f11dc2d02522e376e
brodix78 <brodnikov1950@gmail.com>
2020-04-18 20:16:18 +0400

    Исправить код класс SimpleGenerator.[#227270]

--- chapter_104/src/test/java/ru/job4j/tdd/GeneratorTest.java
+++ chapter_104/src/test/java/ru/job4j/tdd/GeneratorTest.java
@@ -2,23 +2,23 @@ package ru.job4j.tdd;
 
 import org.junit.Test;
 import java.util.HashMap;
+import java.util.NoSuchElementException;
 
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class GeneratorTest {
-/*
     @Test
     public void mainTestWhenAllOK() {
         Generator generator =  new SimpleGenerator();
         HashMap<String, String> args = new HashMap<>();
         args.put("name", "Homer Simpson");
         args.put("subject", "greatest man");
-        assertThat(generator.produce("We are sure that ${name} is the ${subject}."),
+        assertThat(generator.produce("We are sure that ${name} is the ${subject}.", args),
                 is("We are sure that Homer Simpson is the greatest man."));
     }
 
-    @Test
+    @Test(expected = NoSuchElementException.class)
     public void whenToMuchArguments() {
         Generator generator =  new SimpleGenerator();
         HashMap<String, String> args = new HashMap<>();
@@ -26,27 +26,15 @@ public class GeneratorTest {
         args.put("subject", "greatest man");
         args.put("isnot", "fat pig");
         String rsl = null;
-        try {
-            generator.produce("We are sure that ${name} is the ${subject}.");
-        } catch (Exception e) {
-            rsl = e.getMessage();
-        }
-        assertThat(rsl, is("Values not in pattern -\"isnot\""));
+        generator.produce("We are sure that ${name} is the ${subject}.", args);
     }
 
-    @Test
+    @Test(expected = NoSuchElementException.class)
     public void whenNotEnoughArguments() {
-        Generator generator =  new SimpleGenerator();
+        Generator generator = new SimpleGenerator();
         HashMap<String, String> args = new HashMap<>();
         args.put("name", "Homer Simpson");
         String rsl = null;
-        try {
-            generator.produce("We are sure that ${name} is the ${subject}.");
-        } catch (Exception e) {
-            rsl = e.getMessage();
-        }
-        assertThat(rsl, is("Not enough arguments. Need data for keys -\"subject\""));
+        generator.produce("We are sure that ${name} is the ${subject}.", args);
     }
-
- */
 }
