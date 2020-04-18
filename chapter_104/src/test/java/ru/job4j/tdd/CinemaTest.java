4638339687d9b0538ffeabba679091138f671c8a
brodix78 <brodnikov1950@gmail.com>
2020-04-18 15:13:05 +0400

    Видео[#227271]

---
+++ chapter_104/src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -0,0 +1,50 @@
+package ru.job4j.tdd;
+
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
+
+public class CinemaTest {
+
+    @Test
+    public void buy() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        assertThat(ticket, is(new Ticket3D()));
+    }
+
+    @Test
+    public void find() {
+        Cinema cinema = new Cinema3D();
+        cinema.add(new Session3D());
+        List<Session> sessions = cinema.find(session -> true);
+        assertThat(sessions, is(Arrays.asList(new Session3D())));
+    }
+
+    @Test
+    public void nullWhenTryToBuySoldTicket {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        cinema.buy(account, 1, 1, date);
+        Account otherAccount = new AccountCinema();
+        assertNull(cinema.buy(otherAccount, 1, 1, date));
+    }
+
+    @Test
+    public void falseWhenTryToAddSameSession() {
+        Cinema cinema = new Cinema3D();
+        assertThat(cinema.add(new Session3D()), is (true));
+        assertThat(cinema.add(new Session3D()), is (false));
+    }
+}
