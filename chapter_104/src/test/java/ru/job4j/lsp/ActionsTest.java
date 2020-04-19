09dc0f47525308a7f36aebdd24ddbc3ad2c748c9
brodix78 <brodnikov1950@gmail.com>
2020-04-19 09:51:44 +0400

    Парковка машин[#227261]

---
+++ chapter_104/src/test/java/ru/job4j/lsp/ActionsTest.java
@@ -0,0 +1,56 @@
+package ru.job4j.lsp;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ActionsTest {
+
+    Parking park = new SimpleParking();
+    Car car = new Car("X444XX");
+    StringWriter out = new StringWriter();
+    PrintWriter output = new PrintWriter(out);
+    Action addCar = new AddCar(park);
+    Action getCar = new GetCar(park);
+    UserInterface ui = new UserInterface(List.of(addCar, getCar));
+    ui.setOut = out;
+
+    @Before
+    public void start() {
+        LinkedHashMap<String, Car> pk = new LinkedHashMap<>();
+        pk.put("1", null);
+        pk.put("2", null);
+        park.setParking(pk);
+        output.toString();
+    }
+
+    @Test
+    public void addCarByAction () {
+        BufferedReader input = new BufferedReader(new StringReader("X444XX%nn%n"));
+        ui.setIn = input;
+        String exp = String.format("Input car number: %nCaro(y/n): %nParked to place# 1%n");
+        assertThat(addCar.execute(ui), is (true));
+        output.flush();
+        assertThat(out.toString(), is (exp));
+    }
+
+    @Test
+    public void getCarByAction () {
+        BufferedReader input = new BufferedReader(new StringReader ("X444XX%n"));
+        ui.setIn = input;
+        park.add(car);
+        String exp = String.format("Input car number: %nOK%n");
+        assertThat(getCar.execute(ui), is (true));
+        output.flush();
+        assertThat(out.toString(), is (exp));
+    }
+}
---
+++ chapter_104/src/test/java/ru/job4j/lsp/UserInterfaceTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.lsp;
+
+import org.junit.Test;
+import ru.job4j.isp.Action;
+
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.List;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class UserInterfaceTest {
+
+    StringWriter out = new StringWriter();
+    PrintWriter output = new PrintWriter(out);
+
+    @Test
+    public void testMenu() {
+        BufferedReader input = new BufferedReader(new StringReader("3%n"));
+        UserInterface ui = new UserInterface(List.of(new AddCar(new SimpleParking()),
+                new GetCar(new SimpleParking())));
+        ui.setOut = out;
+        ui.setIn = input;
+        String exp = String.format("1. Park a car;%n2. Leave parking;%n3. Exit.%n%nInput your choice (1-3): ");
+        output.flush();
+        assertThat(out.toString(), is(exp));
+    }
+
+
+
+}
