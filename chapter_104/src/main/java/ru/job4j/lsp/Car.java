7119e7679688980b243b4c13f2ce77e6131d6a14
brodix78 <brodnikov1950@gmail.com>
2020-05-14 22:12:27 +0400

    Parking

---
+++ chapter_104/src/main/java/ru/job4j/lsp/Car.java
@@ -0,0 +1,47 @@
+package ru.job4j.lsp;
+
+import java.util.Objects;
+
+public class Car {
+    private String number;
+    private int places;
+
+    public Car(String number) {
+        this.number = number;
+        this.places = 1;
+    }
+
+    public Car(String number, int places) {
+        this.number = number;
+        this.places = places;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public int getPlaces() {
+        return places;
+    }
+
+    public void setPlaces(int places) {
+        this.places = places;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return number.equals(car.number);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+}
