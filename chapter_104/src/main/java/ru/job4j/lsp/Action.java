02d5e5b0ee49bd52dc80b3358c8a905d741f3aea
brodix78 <brodnikov1950@gmail.com>
2020-05-15 21:40:32 +0400

    Parking

--- chapter_104/src/main/java/ru/job4j/lsp/Action.java
+++
@@ -1,6 +0,0 @@
-package ru.job4j.lsp;
-
-public interface Action {
-    boolean execute(UserInterface ui);
-    String actionName();
-}
--- chapter_104/src/main/java/ru/job4j/lsp/Parking.java
+++
@@ -1,6 +0,0 @@
-package ru.job4j.lsp;
-
-public interface Parking {
-    String addCar(Car car);
-    Car getCar(String number);
-}
