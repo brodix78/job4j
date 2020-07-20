68f48431c9918622c80292428bf3e08eb068b086
brodix78 <brodnikov1950@gmail.com>
2020-07-20 10:14:53 +0400

    Thread switcher[1108#304198]

--- chapter_201/src/main/java/ru/job4j/concurrent/Switcher.java
+++ chapter_201/src/main/java/ru/job4j/concurrent/Switcher.java
@@ -4,13 +4,20 @@ import java.util.concurrent.Semaphore;
 
 public class Switcher {
     private static Semaphore semaphore = new Semaphore(1);
-    private static boolean aTurn = true;
+    private static volatile boolean aTurn = true; // (тут моя невнимательность - был уверен, что воткнул volatile)
 
     public static void main(String[] args) throws InterruptedException {
         Thread first = new Thread(
             () -> {
                 while (true) {
                     while (!aTurn) {
+                        // Для равномерного вывода и сомневающихся
+                        // перекинул паузу на верх: "Thread A" всеравно выводится первой
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
                         try {
                             semaphore.acquire();
                         } catch (InterruptedException e) {
@@ -23,34 +30,32 @@ public class Switcher {
                     System.out.println("Thread A");
                     aTurn = false;
                     semaphore.release();
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
                 }
             });
         Thread second = new Thread(
             () -> {
                 while (true) {
+                    //Вторая ветка может проверить флаг первой, и получить разрешение от семафора,
                     while (aTurn) {
                         try {
                             semaphore.acquire();
                         } catch (InterruptedException e) {
                             Thread.currentThread().interrupt();
                         }
+                        //но тут же вернет его если флаг true, поэтому и aTurn играет роль.
                         if (aTurn) {
                             semaphore.release();
                         }
                     }
-                    System.out.println("Thread B");
-                    semaphore.release();
-                    aTurn = true;
                     try {
                         Thread.sleep(1000);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
+                    //Паузу поднял наверх для равномерного вывода
+                    System.out.println("Thread B");
+                    semaphore.release();
+                    aTurn = true;
                 }
             }
     );
